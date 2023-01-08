e7d622023305157ba0902e3b384f1f81155d7323
Danish-Sheikhh <112752050+Danish-Sheikhh@users.noreply.github.com>
2023-01-08 13:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 // API
 app.use("/api/import", ImportData);
 app.use("/api/products", productRoute);
