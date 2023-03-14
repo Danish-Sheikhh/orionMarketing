ec2e84db0c4206ac73695996b37e620974542046
Danish-Sheikhh <112752050+Danish-Sheikhh@users.noreply.github.com>
2023-03-14 18:28:45 +0530

    Add files via upload

---
+++ data/Products.js
@@ -0,0 +1,66 @@
+
+const products = [
+  {
+    
+  name: "Air Conditioner Motors",
+  category:'mt5',
+   image: "/images/ac1.png",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+
+  countInStock: 10,
+  rating: 2,
+  numReviews: 2,
+},
+{
+  
+  name: "Kolkata Motors",
+  category:'mt4',
+  image: "/images/airc1.png",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+  
+  countInStock: 0,
+  rating: 3.5,
+  numReviews: 3,
+},
+{
+  
+  name: "Exhaust Fan Motors",
+  category:'mt3',
+  image: "/images/exhaust.png",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+  
+  countInStock: 10,
+  rating: 5,
+  numReviews: 9,
+},
+{
+  
+  name: "Air Cooler Motors",
+  category:'mt2',
+  image: "/images/kolkata.png",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+  countInStock: 7,
+  rating: 2,
+  numReviews: 2,
+ 
+},
+{
+  
+  name: "General purpose Motors",
+  category:"mt1",
+  image: "/images/fr100.png",
+  description:
+    "Enclosure available are DP (with Velox Fan), TEFC, DPFC, Powder coated sheet Metal Body with aluminum or cast Iron End cover, CSIR, CSCR and PSC are available, Mounting: Standard Foot/Flange/Foot cum Flange, Pressure Die-casted Dynamically Balanced rotor, Pre-Loaded Bearing system for Longer Life, Velox Material centrifugal switch mechanism for trouble free Longer life.",
+  countInStock: 0,
+  rating: 0,
+  numReviews: 0,
+      
+},
+];
+
+export default products;
+
