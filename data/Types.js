bac7470c5723383a2efe6293c2a0c5c78e483286
Danish-Sheikhh <112752050+Danish-Sheikhh@users.noreply.github.com>
2023-03-14 18:31:40 +0530

    Add files via upload

---
+++ data/Types.js
@@ -0,0 +1,110 @@
+const types = [
+    {
+    
+    categoy_id:'mt1',
+    name: "mt1",
+    title: "GP MOTOR (Fr-56)",
+    image: "/images/fr56.png", 
+    description:
+      "Enclosure available are DP (with Velox Fan), TEFC, DPFC, Powder coated sheet Metal Body with aluminum or cast Iron End cover. CSIR, CSCR and PSC are available Mounting: Standard Foot/Flange/Foot cum Flange Pressure Die-casted Dynamically Balanced rotor Pre-Loaded Bearing system for Longer Life. Velox Material centrifugal switch mechanism for trouble free Longer life.",
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+   
+  },
+  {
+    categoy_id:'mt1',
+    name: "mt1",
+    title: "GP MOTOR (Fr-100s)",
+    image: "/images/fr100.png", 
+    description:
+      "Upto 3 HP In 100s Frame Enclosure available are TEFC,Class F Insulation available CSCR & PSC options available Mounting : Standard foot & Flange",
+    countInStock: 5,
+    rating: 4,
+    numReviews: 2,
+
+  },
+  {
+    categoy_id:'mt1',
+    name: "mt1",
+    title: "GP MOTOR (Fr-30)",
+    image: "/images/fr30.png", 
+    description:
+      "Up to ½ HP in Fr-30 Enclosure available are DP. CSIR and PSC options available Mounting : Standard foot In house facility for lamination to ensure optimum electrical characteristics",
+      countInStock: 10,
+      rating: 2,
+      numReviews: 2,
+   },
+  {
+    categoy_id:'mt5',
+    name: "mt5",
+    title: "ROOM AIR CONDITIONER MOTORS",
+    image: "/images/ac1.png",
+    description:
+      "Window Air conditioners are compact designed units making it a challenge to meet up the BEE Star rating in the same. Marathon Electric is the leader in this motor segment vide its best-in-class high efficient & reliable design, catering to the entire spectrum of India-manufactured Window Aircon brands. Customers include Voltas, Blue Star, Lloyd, Panasonic, JCH-Hitachi, Daikin, Godrej, Haier, Croma, EPack, Subros, Amber, Ezentech & many more. Genteq motors for middle-east supply are designed for higher ambient temperature & peak loads. Fr-29 (NEMA 42) in CapCan construction presents one of the most feasible techno-commercial alternatives from Marathon stable, facilitating its customers to be as competitive.",
+    countInStock: 4,
+    rating: 5,
+    numReviews: 2,
+
+  },
+  {
+    categoy_id:'mt5',
+    name: "mt5",
+    title : "SPLIT AIR CONDITIONER ODU MOTORS",
+    image: "/images/ac3.png",
+        description:
+      "Enclosure available are DP (with Velox Fan), TEFC, DPFC, Powder coated sheet Metal Body with aluminum or cast Iron End cover, CSIR, CSCR and PSC are available, Mounting: Standard Foot/Flange/Foot cum Flange, Pressure Die-casted Dynamically Balanced rotor, Pre-Loaded Bearing system for Longer Life, Velox Material centrifugal switch mechanism for trouble free Longer life.",
+    countInStock: 6,
+    rating: 3.5,
+    numReviews: 1,   
+  },
+  {
+    categoy_id:'mt2',
+    name: "mt2",
+    title : "AIR COOLER 1",
+    image: "/images/airc1.png",
+    description:
+      "Upto 3 HP In 100s Frame Enclosure available are TEFC,Class F Insulation available CSCR & PSC options available Mounting : Standard foot & Flange",
+    countInStock: 11,
+    rating: 4,
+    numReviews: 1,
+
+  },{
+    categoy_id:'mt2',
+    name: "mt2",
+    title: "AIR COOLER 2",
+    image: "/images/ac3.png",
+    description:
+      "Upto 3 HP In 100s Frame Enclosure available are TEFC,Class F Insulation available CSCR & PSC options available Mounting : Standard foot & Flange",
+    countInStock: 15,
+    rating: 2.5,
+    numReviews: 2,
+
+  },
+  {
+    categoy_id:'mt3',
+    name: "mt3",
+    title: "EXHAUST MOTORS",
+    image: "/images/exhaust.png",
+    description:
+      "Upto 3 HP In 100s Frame Enclosure available are TEFC,Class F Insulation available CSCR & PSC options available Mounting : Standard foot & Flange",
+    countInStock: 15,
+    rating: 2.5,
+    numReviews: 2,
+
+  },
+  {
+    categoy_id:'mt4',
+    name: "mt4" ,
+    title:"KOLKATA MOTORS",
+    image: "/images/kolkata.png",
+    description:
+      "Upto 3 HP In 100s Frame Enclosure available are TEFC,Class F Insulation available CSCR & PSC options available Mounting : Standard foot & Flange",
+    countInStock: 12,
+    rating: 1,
+    numReviews: 3,
+
+  },
+];
+
+export default types;
